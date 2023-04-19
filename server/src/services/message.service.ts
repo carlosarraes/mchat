7cd3acd437a3e377c8799d2a2040faa2de3f2d77
carraes <carraeshb@gmail.com>
2023-04-19 15:13:03 -0300

    feat(back): add dayjs plugin to correct server time

--- server/src/services/message.service.ts
+++ server/src/services/message.service.ts
@@ -1,12 +1,17 @@
 import { messageModel } from '../models/message.model'
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 const getAll = async () => {
   const messages = await messageModel.getAll()
   const returnMessages = messages.map((message) => {
     return {
       ...message,
-      timestamp: dayjs(message.timestamp).format('DD/MM HH:mm'),
+      timestamp: dayjs(message.timestamp).tz('America/Sao_Paulo').format('DD/MM HH:mm'),
     }
   })
 
