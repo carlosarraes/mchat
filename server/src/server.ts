7d290672225530c366d222a9e4f526e3b7b4b366
Carlos <carraeshb@gmail.com>
2023-04-19 14:33:59 -0300

    Connect front and backend (#4)
    * feat(front): update endpoint to real backend
    
    * feat(back): add real front address to cors
    
    * chore: add types/cors pkg
    
    * feat(back): Dockerfile to backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ app.use('/messages', messageRoutes)
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://mchat-ca.vercel.app'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['my-custom-header'],
     credentials: true,
