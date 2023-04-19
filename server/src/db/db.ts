38536acccb733b628b8d9aced0f5cfce34293439
carraes <carraeshb@gmail.com>
2023-04-19 10:08:00 -0300

    refactor(back): db config, for better clarity, configure logger

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,18 +1,16 @@
 import { drizzle } from 'drizzle-orm/planetscale-serverless'
+import dotenv from 'dotenv'
+dotenv.config()
 
-let connection: any
-
-async function connectToDatabase() {
+const db = (async () => {
   const { connect } = await import('@planetscale/database')
-  connection = connect({
+  const connection = connect({
     host: process.env['DATABASE_HOST'],
     username: process.env['DATABASE_USERNAME'],
     password: process.env['DATABASE_PASSWORD'],
   })
-}
 
-connectToDatabase()
+  return drizzle(connection, { logger: true })
+})()
 
-const db = drizzle(connection)
-
-export default db
+export { db }
