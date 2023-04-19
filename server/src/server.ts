75a04568b676f31c6663be3245fac5c0f3a97911
carraes <carraeshb@gmail.com>
2023-04-18 22:06:35 -0300

    feat(back): basic io config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,30 @@
 import express from 'express'
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
 import morgan from 'morgan'
 
-const app = express()
 const port = process.env.PORT || 3000
+const app = express()
+const server = createServer(app)
 
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.get('/', (_, res) => {
-  res.status(200).json({ message: 'Hello CI/CD :)!' })
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
 })
 
-app.listen(port, () => {
+io.on('connection', (socket: Socket) => {
+  console.log(`User connected: ${socket.id}`)
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`)
+  })
+})
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
