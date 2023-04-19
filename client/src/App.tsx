aab1a3a3e492f83a992f80e5fa4a1dec60819ded
carraes <carraeshb@gmail.com>
2023-04-18 22:08:43 -0300

    feat(front): basic conn to socket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react'
+import socketIOClient from 'socket.io-client'
+import type * as SocketIoClient from 'socket.io-client'
+const ENDPOINT = 'http://localhost:3000/'
 function App() {
+  const [socket, setSocket] = useState<SocketIoClient.Socket | null>(null)
+  useEffect(() => {
+    setSocket(socketIOClient(ENDPOINT))
+    return () => {
+      if (socket) socket.disconnect()
+    }
+  }, [])
   return (
     <div>
       <h1 className="text-4xl">Hello</h1>
@@ -6,4 +17,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
