1f4bea534a7211089c79a3952a358542b4f17e2b
carraes <carraeshb@gmail.com>
2023-04-19 00:47:34 -0300

    feat(front): useRef to scroll down once new msg arrives

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { receivedData } from '../App'
 
 type ChatBoxProps = {
@@ -5,8 +6,14 @@ type ChatBoxProps = {
 }
 
 const ChatBox = ({ messages }: ChatBoxProps) => {
+  const chatBoxRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (chatBoxRef.current) chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight
+  }, [messages])
+
   return (
-    <section className="flex flex-col flex-grow p-2 overflow-auto">
+    <section ref={chatBoxRef} className="flex flex-col flex-grow p-2 overflow-auto">
       {messages.map(({ message, username }, index) => (
         <p key={index} className={`${index % 2 === 0 && 'bg-slate-100'} p-2`}>
           {`${username}: ${message}`}
